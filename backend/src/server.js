cbc3c649f27cc6e8ec2bf08cfe3f0028de7d3aef
brajeshsaxena01 <brajeshkumarsaxena82@gmail.com>
2024-09-16 14:53:54 +0530

    testing deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
-// const connect = require("./configs/db");
-// const app = require("./index");
-// const port = process.env.PORT;
-// app.listen(port, async () => {
-//   try {
-//     await connect();
-//     console.log("MongoDb connected");
-//   } catch (error) {
-//     console.log(error);
-//   }
-//   console.log(`listening on port ${port}`);
-// });
+const connect = require("./configs/db");
+const app = require("./index");
+const port = process.env.PORT;
+app.listen(port, async () => {
+  try {
+    await connect();
+    console.log("MongoDb connected");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`listening on port ${port}`);
+});
